6318f214ad0832d2644e6ff7a4be7928be397539
Jonas Lundvall <jonlundv@kth.se>
2016-12-04 22:29:41 +0100

    Ändrat i Album, AlbumCollection, Review, SearchOptions, User, Controller, QueryDialog och View

--- DBLab1/src/model/User.java
+++ DBLab1/src/model/User.java
@@ -11,4 +11,18 @@ public class User {
         this.userName = userName;
         
     }
+
+    /**
+     * @return the userName
+     */
+    public String getUserName() {
+        return userName;
+    }
+
+    /**
+     * @param userName the userName to set
+     */
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 }
