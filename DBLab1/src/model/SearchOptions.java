b94768152e9130db25eb71e1d95c277f0f0c58a5
Jonas Lundvall <jonlundv@kth.se>
2016-12-04 22:13:20 +0100

    Ändrat i Album, AlbumCollection, Review, SearchOptions, User, Controller, QueryDialog och View

--- DBLab1/src/model/SearchOptions.java
+++ DBLab1/src/model/SearchOptions.java
@@ -1,12 +1,12 @@
 package model;
 
-import org.jetbrains.annotations.Contract;
+//import org.jetbrains.annotations.Contract;
 
 /**
  * Created by Martin on 2016-12-04.
  */
 public enum SearchOptions {
-    TITLE("title"), ARTIST("name"), GENRE("genre");
+    TITLE("title"), ARTIST("name"), GENRE("genre"), ALBUM("album");
     private final String searchQuery;
 
     SearchOptions(String s) {
